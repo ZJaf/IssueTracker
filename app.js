40e9f52248f4ef4246796f90e298df9293906f80
HJ <haniejafarii@gmail.com>
2017-10-01 21:18:40 -0400

    Close and Delete Btn work now

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+//---- Deleting the Issue
+function removeIssue(id) {
+    $('#' + id).remove();
+}
+
+//---- Closing the Issue
+function closeIssue(id) {
+    $('#' + id + '-status').html('Closed');
+}
+
 $(document).ready(function() {
+    var list = [];
     //-- Submiting a ticket - Function:
     $('form').on('submit', function(e) {
         e.preventDefault();
@@ -7,31 +18,27 @@ $(document).ready(function() {
         issue.assignee = $('#exampleInputAssignee').val();
         issue.sev = $('#exampleFormControlSelect1').val();
         issue.id = chance.guid(); //-- Generate Unique ID
-        // console.log(issue.text, issue.assignee);   //-- Testing object with Console
-
 
         //--  generating Ticket
         var issueTemplate = `
-        <section class="ticket">
+        <section class="ticket" id="${issue.id}">
         <div>
             <p>Issue ID:${issue.id}</p>
-            <label class="btn btn-primary btn-sm" Style="background-color:blue; border-color:blue "> Open </label>
+            <label id="${issue.id}-status" class="btn btn-primary btn-sm" id="label" Style="background-color:blue; border-color:blue "> Open </label>
             <h3>${issue.text}</h4>
-                <p><i class="fa fa-clock-o" aria-hidden="true"></i>${issue.sev}</p>
-                <p><i class="fa fa-user-circle" aria-hidden="true"></i>${issue.assignee}</p>
-                <button type="button " class="btn btn-warning btn-lg ">Close</button>
-                <button type="button " class="btn btn-danger btn-lg ">Delete</button>
+            <p><i class="fa fa-clock-o" aria-hidden="true"></i>${issue.sev}</p>
+            <p><i class="fa fa-user-circle" aria-hidden="true"></i>${issue.assignee}</p>
+            <input type="submit" class="btn btn-warning btn-lg" value="Close" onclick="closeIssue('${issue.id}')"/>
+            <button class="btn btn-danger btn-lg" onclick="removeIssue('${issue.id}')">Delete</button>
         </div>
         </section>
         `
-            //-- add ticket to the list of Issues
+        list.push(issue.id);
+        //-- add ticket to the list of Issues
         $('#issueList').append(issueTemplate);
         //--- Empty the fields
         $('#exampleInputIssue').val('');
         $('#exampleInputAssignee').val('');
-        //--- ?? May have a better way !
-        $('#exampleFormControlSelect1').val('');
-
+        $('#exampleFormControlSelect1').val(''); //--- ?? May have a better way !
     })
-
 })
